3d2a6593af79b373d82df80e7f6ad4f4ec34e339
sepehrtvk <58414277+sepehrtvk@users.noreply.github.com>
2019-12-21 00:50:15 +0330

    final version

---
+++ Dozd_o_Police/cmake-build-debug/CMakeFiles/Dozd_o_Police.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/sepehr/Desktop/Dozd_o_Police/main.c" "/Users/sepehr/Desktop/Dozd_o_Police/cmake-build-debug/CMakeFiles/Dozd_o_Police.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Dozd_o_Police/cmake-build-debug/CMakeFiles/Dozd_o_Police.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dozd_o_Police.dir/main.c.o"
+  "Dozd_o_Police"
+  "Dozd_o_Police.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dozd_o_Police.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
